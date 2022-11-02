e872107eadba981c0576e59cf6dc0dccec2671c0
Rafael Kolbe Mendes <rafa.kolbe@hotmail.com>
2022-11-02 15:47:07 -0300

    finished db structure

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+	const user = await prisma.user.create({
+		data: {
+			name: "John Doe",
+			email: "john.doe@hotmail.com",
+			avatarUrl: "http://github.com/rafael-kolbe.png",
+		},
+	});
+
+	const pool = await prisma.pool.create({
+		data: {
+			title: "Example Pool",
+			code: "BOL123",
+			ownerId: user.id,
+
+			participants: {
+				create: {
+					userId: user.id,
+				},
+			},
+		},
+	});
+
+	await prisma.game.create({
+		data: {
+			date: "2022-11-26T15:00:00.305Z",
+			firstTeamCountryCode: "DE",
+			secondTeamCountryCode: "BR",
+		},
+	});
+
+	await prisma.game.create({
+		data: {
+			date: "2022-11-25T15:00:00.305Z",
+			firstTeamCountryCode: "BR",
+			secondTeamCountryCode: "AR",
+
+			guesses: {
+				create: {
+					firstTeamPoints: 2,
+					secondTeamPoints: 1,
+
+					participant: {
+						connect: {
+							userId_poolId: {
+								userId: user.id,
+								poolId: pool.id,
+							},
+						},
+					},
+				},
+			},
+		},
+	});
+}
+
+main();
