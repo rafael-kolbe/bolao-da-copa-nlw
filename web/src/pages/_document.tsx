20db86a2d5d70e05463848ca5a5112428e0441b6
Rafael Kolbe Mendes <rafa.kolbe@hotmail.com>
2022-11-02 20:08:40 -0300

    main page layout finished

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
                     rel="stylesheet"
                 />
             </Head>
-            <body className="bg-gray-900">
+            <body className="bg-gray-900 bg-app bg-cover">
                 <Main />
                 <NextScript />
             </body>
