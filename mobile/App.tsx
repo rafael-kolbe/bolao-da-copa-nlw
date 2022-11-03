3c5b5fee24a35afe0e87a3f5204010e3d85f352d
Rafael Kolbe Mendes <rafa.kolbe@hotmail.com>
2022-11-03 12:58:24 -0300

    authContext, hook and first page layout finished

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,31 +1,32 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import {
-	useFonts,
-	Roboto_400Regular,
-	Roboto_500Medium,
-	Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { Loading } from "./src/components/Loading";
-import { SignIn } from "./src/screens/SignIn";
+    useFonts,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
-import { THEME } from "./src/styles/theme";
+import { AuthContextProvider } from './src/contexts/AuthContext';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		Roboto_400Regular,
-		Roboto_500Medium,
-		Roboto_700Bold,
-	});
+    const [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium,
+        Roboto_700Bold,
+    });
 
-	return (
-		<NativeBaseProvider theme={THEME}>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="transparent"
-				translucent
-			/>
+    return (
+        <NativeBaseProvider theme={THEME}>
+            <AuthContextProvider>
+                <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-			{fontsLoaded ? <SignIn /> : <Loading />}
-		</NativeBaseProvider>
-	);
+                {fontsLoaded ? <SignIn /> : <Loading />}
+            </AuthContextProvider>
+        </NativeBaseProvider>
+    );
 }
