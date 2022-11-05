d8a42e04a5f0b0682aab84d302e3929b55c39fb2
Rafael Kolbe Mendes <rafa.kolbe@hotmail.com>
2022-11-05 20:41:37 -0300

    mobile almost finished

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -1,14 +1,42 @@
-import { Box } from 'native-base';
+import { useState, useEffect } from 'react';
+import { Box, useToast } from 'native-base';
+
+import { api } from '../services/api';
+
+import { Game, GameProps } from '../components/Game';
 
 interface Props {
-  poolId: string;
+    poolId: string;
 }
 
 export function Guesses({ poolId }: Props) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [games, setgames] = useState([]);
 
-  return (
-    <Box>
+    const toast = useToast();
 
-    </Box>
-  );
+    async function fetchGames() {
+        try {
+            setIsLoading(true);
+
+            const response = await api.get(`/pools/${poolId}/games`);
+            console.log(response.data.games);
+        } catch (error) {
+            console.log(error);
+
+            toast.show({
+                title: 'Não foi possível carregar os jogos',
+                placement: 'top',
+                bgColor: 'red.500',
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchGames();
+    }, [poolId]);
+
+    return <Box></Box>;
 }
--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,49 @@
-import { VStack, Icon } from 'native-base';
+import { useCallback, useState } from 'react';
+
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { api } from '../services/api';
 
 import { Header } from '../components/Header';
 import { Button } from '../components/Button';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
     const { navigate } = useNavigation();
+    const [isLoading, setIsLoading] = useState(true);
+    const [pools, setPools] = useState<PoolCardProps[]>([]);
+
+    const toast = useToast();
+
+    async function fetchPools() {
+        try {
+            setIsLoading(true);
+            const response = await api.get('/pools');
+            setPools(response.data.pools);
+        } catch (error) {
+            console.log(error);
+
+            toast.show({
+                title: 'Não foi possível carregar os bolões',
+                placement: 'top',
+                bgColor: 'red.500',
+            });
+
+            throw error;
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            fetchPools();
+        }, []),
+    );
 
     return (
         <VStack flex={1} bgColor="gray.900">
@@ -18,6 +55,24 @@ export function Pools() {
                     onPress={() => navigate('find')}
                 />
             </VStack>
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <FlatList
+                    data={pools}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <PoolCard
+                            data={item}
+                            onPress={() => navigate('details', { id: item.id })}
+                        />
+                    )}
+                    px={5}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 10 }}
+                    ListEmptyComponent={() => <EmptyPoolList />}
+                />
+            )}
         </VStack>
     );
 }
